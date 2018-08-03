fd28701774a0f144aca83950791de3899986d3a3
MikeO <ozman4@gmail.com>
2018-08-03 17:19:07 -0500

    adding comment

--- Leetcode/Reverse-String.py
+++ Leetcode/Reverse-String.py
@@ -1,5 +1,6 @@
 #Problem: 344
-#Using slicing
+#Using slicing, this is a really interesting python caveat by making the step
+# size -1 we go in the reverse direction
 class Solution(object):
     def reverseString(self, s):
         return s[::-1]
